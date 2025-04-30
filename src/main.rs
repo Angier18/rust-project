76f750e7d9fdbd36d88811f9a4ff949244f506cf
Angier18 <157068304+Angier18@users.noreply.github.com>
2025-04-30 08:55:22 +0000

    Wire main to load CSV, filter stimulants, compute & print increases

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use rust_project::{analysis, csv_reader};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let all = csv_reader::read_records("data/Drug_overdose_death_rates__by_drug_type__sex__age__race__and_Hispanic_origin__United_States.csv")?;
+    let stim = analysis::filter_stimulants(&all);
+    let increases = analysis::increase_since_2010(&stim);
+
+    let mut inc_vec: Vec<_> = increases.into_iter().collect();
+    inc_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    println!("Race/Ethnicity                     Δ Rate since 2010");
+    println!("-----------------------------------------------");
+    for (race, delta) in inc_vec {
+        println!("{:<30} {:+.2}", race, delta);
+    }
+    Ok(())
 }
