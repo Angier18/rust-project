5fe7c07a84f465613e0f652f0de1516d19bb4608
Angier18 <157068304+Angier18@users.noreply.github.com>
2025-04-30 08:54:17 +0000

    added increase function and print results in main

--- src/analysis.rs
+++ src/analysis.rs
@@ -19,3 +19,23 @@ pub fn rate_by_race_in_year(
     } 
     map 
 }
+
+pub fn increase_since_2010(
+    stimulant_records: &[OverdoseRecord],
+) -> HashMap<String, f64> {
+    let mut years: Vec<u16> = stimulant_records.iter().map(|r| r.year).collect();
+    years.sort_unstable();
+    years.dedup();
+    let &latest_year = years.last().expect("No years in data");
+
+    let base_map = rate_by_race_in_year(stimulant_records, 2010);
+    let latest_map = rate_by_race_in_year(stimulant_records, latest_year);
+
+    let mut diff = HashMap::new();
+    for (race, &latest_rate) in &latest_map {
+        if let Some(&base_rate) = base_map.get(race) {
+            diff.insert(race.clone(), latest_rate - base_rate);
+        }
+    }
+    diff
+}
