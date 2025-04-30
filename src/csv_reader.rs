ff5d640f45de223c2200e7f21518c64c197b995a
Angier18 <157068304+Angier18@users.noreply.github.com>
2025-04-30 19:46:54 +0000

    I needed to change it to so that it will ignore empty rows

--- src/csv_reader.rs
+++ src/csv_reader.rs
@@ -7,6 +7,12 @@ pub fn read_records(path: &str) -> Result<Vec<OverdoseRecord>, Box<dyn Error>> {
         .has_headers(true)
         .from_path(path)?;
 
+    let headers = reader.headers()?.clone();
+    let year_i  = headers.iter().position(|h| h == "YEAR").unwrap();
+    let label_i = headers.iter().position(|h| h == "STUB_LABEL").unwrap();
+    let name_i  = headers.iter().position(|h| h == "STUB_NAME").unwrap();
+    let rate_i  = headers.iter().position(|h| h == "ESTIMATE").unwrap();
+
     let mut records = Vec::new();
     for result in reader.deserialize(){
         let record: OverdoseRecord = result?;
