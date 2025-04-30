2ff4f99f6b1d443a3cc5cf991957f6cc34804b8f
Angier18 <157068304+Angier18@users.noreply.github.com>
2025-04-30 08:16:23 +0000

    fix import and return records

--- src/csv_reader.rs
+++ src/csv_reader.rs
@@ -1,6 +1,15 @@
 use crate::data_model::OverdoseRecord;
+use csv::ReaderBuilder;
 use std::error::Error;
 
 pub fn read_records(path: &str) -> Result<Vec<OverdoseRecord>, Box<dyn Error>> {
-    unimplemented!()
+    let mut reader = ReaderBuilder::new()
+        .has_headers(true)
+        .from_path(path)?;
+
+    let mut records = Vec::new();
+    for result in reader.deserialize(){
+        let record: OverdoseRecord = result?;
+        records.push(record);}
+    Ok(records)
 }
