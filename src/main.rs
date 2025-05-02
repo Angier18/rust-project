6e754545bbc2945a4cdf67173a6f16d9c9ab5520
Angier18 <157068304+Angier18@users.noreply.github.com>
2025-05-02 01:15:17 +0000

    I needed to change the code so that it works with the new code in csv_reader

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use rust_project::{analysis, csv_reader};
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let path = "data/Drug_overdose_death_rates__by_drug_type__sex__age__race__and_Hispanic_origin__United_States.csv";
     let all = csv_reader::read_records(path)?;
-
     {
         let mut labels: Vec<_> = all.iter()
             .map(|r| r.drug_type.clone())
@@ -15,3 +14,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             eprintln!("  {}", lbl);
         }
     }
+    let stim = analysis::filter_stimulants(&all);
+
+    let increases = analysis::increase_since_2010(&stim);
+    let mut inc_vec: Vec<_> = increases.into_iter().collect();
+    inc_vec.sort_by(|a, b| b.1.partial_cmp(&a.1).unwrap());
+
+    println!("Race/Ethnicity                     Δ Rate since 2010");
+    println!("-----------------------------------------------");
+    for (race, delta) in inc_vec {
+        println!("{:<30} {:+.2}", race, delta);
+    }
+
+    Ok(())
+}
